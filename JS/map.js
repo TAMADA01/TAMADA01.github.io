e38896bbbdb106124c780e92a0cef23420bd072a
TAMADA01 <114475437+TAMADA01@users.noreply.github.com>
2024-04-14 12:04:42 +0300

    Fixed Map

--- JS/map.js
+++ JS/map.js
@@ -1,28 +1,19 @@
-initMap();
-
 async function initMap() {
-    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
     await ymaps3.ready;
-
+    
     const {YMap, YMapDefaultSchemeLayer} = ymaps3;
-
-    // Иницилиазируем карту
+    
     const map = new YMap(
-        // Передаём ссылку на HTMLElement контейнера
         document.getElementById('map'),
-
-        // Передаём параметры инициализации карты
         {
             location: {
-                // Координаты центра карты
-                center: [37.588144, 55.733842],
-
-                // Уровень масштабирования
-                zoom: 10
+                center: [37.225844, 56.741702],
+                zoom: 15
             }
         }
     );
-
-    // Добавляем слой для отображения схематической карты
+    
     map.addChild(new YMapDefaultSchemeLayer());
 }
+
+setTimeout(initMap, 1000)
